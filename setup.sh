229eb10c356da3bf060bfbd16b7c3a8e0bc50b63
JC Balcita <jbalcita@cj.com>
2017-04-04 17:43:44 +0000

    cleanup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# versioning
-# setup rpm build environment (_top)
-
 sudo yum install -y git gcc autoconf pam-devel rpm-build
 mkdir -p ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 echo '%_topdir      %(echo $HOME)/rpmbuild' > ~/.rpmmacros 
