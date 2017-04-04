8d13e8a0a94011a481c46e7215de63a0cac44c83
Greg Wiley <gwiley@cj.com>
2017-04-04 11:51:19 -0700

    change package script to github

--- package-rpm.sh
+++ package-rpm.sh
@@ -7,12 +7,12 @@
 VERSION=0.0.1
 
 git archive \
-   --remote git@gitlab.cj.com:operations-and-delivery/dual-control.git \
+   --remote git@github.com:cjdev/dual-control.git \
    --prefix dual-control-${VERSION}/ \
    --format tar.gz \
    -o ~/rpmbuild/SOURCES/dual-control-${VERSION}.tar.gz \
    release-${VERSION}
-   
+
 rpmbuild -ba $(dirname $0)/dual-control.spec
 
 cp ~/rpmbuild/RPMS/x86_64/dual-control-0.0.1-0.x86_64.rpm $(dirname $0)/
